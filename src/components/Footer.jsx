c08103c11ae1c57124e7a6cb7cd1034e0865a230
KevinLemein <klemein15@gmail.com>
2025-09-01 16:39:18 +0300

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,33 +19,25 @@ const Footer = () => {
             <div className="bg-white py-8">
                 <div className="max-w-6xl mx-auto px-4">
                     <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-6 items-center justify-items-center">
-                        {/* Replace these divs with your actual imported images */}
                         <div className="h-16 flex items-center justify-center">
-                            {/* <img src={kenyaRugbyUnionLogo} alt="Kenya Rugby Union" className="h-12 object-contain" /> */}
                             <div className="text-black font-bold text-sm">Kenya Rugby Union</div>
                         </div>
                         <div className="h-16 flex items-center justify-center">
-                            {/* <img src={ministryLogo} alt="Ministry of Youth Affairs and Sports" className="h-12 object-contain" /> */}
                             <div className="text-black font-bold text-sm text-center">Ministry of Youth Affairs</div>
                         </div>
                         <div className="h-16 flex items-center justify-center">
-                            {/* <img src={yalantaLogo} alt="Yalanta HELA" className="h-12 object-contain" /> */}
-                            <div className="text-black font-bold text-sm">Yalanta HELA</div>
+                            <div className="text-black font-bold text-sm">Talanta HELA</div>
                         </div>
                         <div className="h-16 flex items-center justify-center">
-                            {/* <img src={umbroLogo} alt="Umbro" className="h-12 object-contain" /> */}
                             <div className="text-black font-bold text-sm">umbro</div>
                         </div>
                         <div className="h-16 flex items-center justify-center">
-                            {/* <img src={sportPesaLogo} alt="SportPesa" className="h-12 object-contain" /> */}
                             <div className="text-black font-bold text-sm">SportPesa</div>
                         </div>
                         <div className="h-16 flex items-center justify-center">
-                            {/* <img src={blockSportLogo} alt="BLOCKSPORT" className="h-12 object-contain" /> */}
                             <div className="text-black font-bold text-sm">BLOCKSPORT</div>
                         </div>
                         <div className="h-16 flex items-center justify-center">
-                            {/* <img src={tikoHubLogo} alt="TikoHUB" className="h-12 object-contain" /> */}
                             <div className="text-black font-bold text-sm">TikoHUB</div>
                         </div>
                     </div>
@@ -55,7 +47,7 @@ const Footer = () => {
             {/* Main Footer */}
             <div className="grid md:grid-cols-3 gap-0 relative">
                 {/* Logo Section - Black */}
-                <div className="bg-black p-8 flex flex-col items-center justify-center min-h-[400px]">
+                <div className="bg-black p-8 flex flex-col items-center justify-center min-h-[500px] clip-black">
                     <div className="mb-8">
                         <div className="flex items-center justify-center mb-6">
                             <div className="w-20 h-20 bg-red-600 rounded-full flex items-center justify-center">
@@ -86,8 +78,10 @@ const Footer = () => {
                 </div>
 
                 {/* Links Section - Red */}
-                <div className="bg-red-600 p-8 text-white min-h-[400px] relative">
-                    <div className="grid grid-cols-2 gap-8 mb-16">
+                {/*<div className="bg-red-600 p-8 text-white min-h-[500px] relative clip-red">*/}
+                <div className="bg-red-600 pt-24 px-8 pb-8 text-black min-h-[500px] relative clip-red">
+
+                <div className="grid grid-cols-2 gap-8 mb-16">
                         <div className="flex flex-col space-y-4">
                             <a href="#" className="hover:text-red-200 transition-colors text-lg">Home</a>
                             <a href="#" className="hover:text-red-200 transition-colors text-lg">Calendar</a>
@@ -104,21 +98,22 @@ const Footer = () => {
                         </div>
                     </div>
 
-                    {/* Copyright for desktop - positioned at bottom center of red section */}
+                    {/* Copyright for desktop */}
                     <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 hidden md:block">
-                        <p className="text-white text-center">© 2025 Safari sevens. All Rights Reserved.</p>
+                        <p className="text-black text-center">© 2025 Safari sevens. All Rights Reserved.</p>
                     </div>
                 </div>
 
                 {/* Contact Section - Green */}
-                <div className="bg-green-600 p-8 text-white min-h-[400px] relative">
-                    <div className="space-y-2">
+                {/*<div className="bg-green-600 p-8 text-white min-h-[500px] relative clip-green">*/}
+                <div className="bg-green-600 pt-32 px-8 pb-8 text-black min-h-[500px] relative clip-green">
+                <div className="space-y-2">
                         <h4 className="font-bold text-xl mb-4">Mombasa, Kenya</h4>
                         <p className="text-lg">+254727991993</p>
                         <p className="text-lg">+254704069264</p>
                     </div>
 
-                    {/* Copyright for mobile - positioned at bottom center of green section */}
+                    {/* Copyright for mobile */}
                     <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 md:hidden">
                         <p className="text-white text-center">© 2025 Safari sevens. All Rights Reserved.</p>
                     </div>
